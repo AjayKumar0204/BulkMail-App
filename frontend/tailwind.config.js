3a70bcbb95fb6f6225f50389804b48cc7fb92529
Ajaykumar0204 <ajaykumaraj0204@gmail.com>
2025-03-03 21:00:19 +0530

    updated all files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensure Tailwind scans the right files
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
